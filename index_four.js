de54ef047bfe3e2371195d4e8c63ca221004e9d3
Samuel Njenga <snjenga704@gmail.com>
2023-08-08 20:19:16 +0300

    Feat: add index_four.js

---
+++ index_four.js
@@ -0,0 +1,11 @@
+// Exponent operator
+const byte = 2 ** 10;
+console.log(byte);
+// Same as:
+console.log(Math.pow(2, 10));
+
+//Spread operator
+const a = [12, 14];
+const b = [25, 26];
+const c = [...a, ...b];
+console.log(c);
