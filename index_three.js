24557bb83dd9beba40a7fa2d14021de9c9f77ded
Samuel Njenga <snjenga704@gmail.com>
2023-08-08 20:14:02 +0300

    Feat: add index_three.js

---
+++ index_three.js
@@ -0,0 +1,10 @@
+// Multiline string
+console.log(`This is a 
+multiline string
+written in JavaScript
+`);
+
+// Template strings
+const library = "React Js";
+const message = `One of the frontend libraries I use is ${library}`;
+console.log(message);
