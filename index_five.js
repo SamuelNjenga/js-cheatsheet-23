db7f33c851e8b92c1ec310f373a610e8008fc4e1
Samuel Njenga <snjenga704@gmail.com>
2023-08-08 20:28:32 +0300

    Feat: add index_five.js

---
+++ index_five.js
@@ -0,0 +1,10 @@
+// String includes()
+console.log("javascript".includes("sc"));
+console.log("javascript".includes("sp"));
+
+// String startsWith()
+console.log("javascript".startsWith("ja"));
+console.log("javascript".startsWith("av"));
+
+// String repeat()
+console.log("java".repeat(3));
