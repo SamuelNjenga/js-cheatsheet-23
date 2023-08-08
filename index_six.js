4c98a9a9016741d9ea1fac893cbf79175a4bd601
Samuel Njenga <snjenga704@gmail.com>
2023-08-08 20:41:44 +0300

    Feat: add index_six.js

---
+++ index_six.js
@@ -0,0 +1,12 @@
+// Destructuring array
+let [a, b, c] = [33, 44, 55];
+console.log(a);
+console.log(b);
+console.log(c);
+
+// Destructuring object
+let obj = { j: 31, k: 41, l: 51 };
+let { j, k, l } = obj;
+console.log(j);
+console.log(k);
+console.log(l);
